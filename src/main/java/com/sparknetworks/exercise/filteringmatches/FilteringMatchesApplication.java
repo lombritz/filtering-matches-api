89a1106b748fdadea23b0db7ad4315eddda31968
Jaime Rojas <jaimitorojas@gmail.com>
2020-02-15 20:13:15 -0400

    Basic Spring Boot project

---
+++ src/main/java/com/sparknetworks/exercise/filteringmatches/FilteringMatchesApplication.java
@@ -0,0 +1,13 @@
+package com.sparknetworks.exercise.filteringmatches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilteringMatchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilteringMatchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparknetworks/exercise/filteringmatches/FilteringMatchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparknetworks.exercise.filteringmatches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilteringMatchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
